e8c150824049ab33f445fd350db78a0e8f869d65
Rach17 <rachediabderrahmane17@gmail.com>
2024-01-20 00:44:27 +0100

    animation

--- main.c
+++ main.c
@@ -17,35 +17,32 @@ bool editMode3 = false;
 bool editMode4 = false;
 bool editMode5 = false;
 
-
 char id[50];
 char name[50];
 char avrage[50];
 
 student students[12] = {
-        { "Alice",2, 85.5},
-        { "Bob",1, 92.0},
-        { "Alice",2, 85.5},
-        { "Bob",1, 92.0},
-        { "Alice",2, 85.5},
-        { "Bob",1, 92.0},
-        { "Alice",2, 85.5},
-        { "Bob",1, 92.0},
-        { "Alice",2, 85.5},
-        { "Bob",1, 92.0},
-        { "Alice",2, 85.5},
-        { "Bob",1, 92.0},
-        // Add more student data as needed
-    };
+    {"Alice", 2, 85.5},
+    {"Bob", 1, 92.0},
+    {"Alice", 2, 85.5},
+    {"Bob", 1, 92.0},
+    {"Alice", 2, 85.5},
+    {"Bob", 1, 92.0},
+    {"Alice", 2, 85.5},
+    {"Bob", 1, 92.0},
+    {"Alice", 2, 85.5},
+    {"Bob", 1, 92.0},
+    {"Alice", 2, 85.5},
+    {"Bob", 1, 92.0},
+    // Add more student data as needed
+};
 
 void SaveFormData(student *formData);
 void insert_form();
 void search_form();
 void delete_form();
 void DrawStudentTable();
-
-
-
+void search_visualisation(int x);
 
 // Function to draw content for Page 0
 void home_page()
@@ -199,7 +196,7 @@ void insert_form()
     GuiSetStyle(LABEL, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
 
     // Check if mouse is inside the first text box
-    if (IsMouseInsideTextBox((Rectangle){ labelWidth, 120, inputWidth, 20}))
+    if (IsMouseInsideTextBox((Rectangle){labelWidth, 120, inputWidth, 20}))
     {
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
@@ -210,7 +207,7 @@ void insert_form()
     }
 
     // Check if mouse is inside the second text box
-    if (IsMouseInsideTextBox((Rectangle){2 * labelWidth + 10 +inputWidth , 120, inputWidth, 25}))
+    if (IsMouseInsideTextBox((Rectangle){2 * labelWidth + 10 + inputWidth, 120, inputWidth, 25}))
     {
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
@@ -220,7 +217,7 @@ void insert_form()
         }
     }
 
-    if (IsMouseInsideTextBox((Rectangle){ 3 * labelWidth + 10 + 2 * inputWidth , 120, inputWidth, 25}))
+    if (IsMouseInsideTextBox((Rectangle){3 * labelWidth + 10 + 2 * inputWidth, 120, inputWidth, 25}))
     {
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
@@ -239,16 +236,16 @@ void insert_form()
 
     start += labelWidth + inputWidth + 10;
 
-    rec_label = (Rectangle) {start, 120, labelWidth, 20};
-    rec_input = (Rectangle) {start + labelWidth, 120, inputWidth, 20};
+    rec_label = (Rectangle){start, 120, labelWidth, 20};
+    rec_input = (Rectangle){start + labelWidth, 120, inputWidth, 20};
 
     GuiLabel(rec_label, "Name:");
     GuiTextBox(rec_input, name, 50, editMode2);
 
     start += labelWidth + inputWidth + 10;
 
-    rec_label = (Rectangle) {start, 120, labelWidth, 20};
-    rec_input = (Rectangle) {start + labelWidth, 120, inputWidth, 20};
+    rec_label = (Rectangle){start, 120, labelWidth, 20};
+    rec_input = (Rectangle){start + labelWidth, 120, inputWidth, 20};
 
     GuiLabel(rec_label, "Avrage:");
     GuiTextBox(rec_input, avrage, 50, editMode3);
@@ -267,8 +264,7 @@ void insert_form()
     }
 }
 
-
-//Search Form
+// Search Form
 void search_form()
 {
     student std = {0};
@@ -280,14 +276,13 @@ void search_form()
     GuiSetStyle(LABEL, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
 
     // Check if mouse is inside the first text box
-    if (IsMouseInsideTextBox((Rectangle){ labelWidth, 120, inputWidth, 20}))
+    if (IsMouseInsideTextBox((Rectangle){labelWidth, 120, inputWidth, 20}))
     {
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
             editMode4 = !editMode5; // Toggle edit mode for TextBox 1
         }
     }
-
 
     // Draw text boxes
     int start = 0;
@@ -297,17 +292,20 @@ void search_form()
     GuiTextBox(rec_input, id, 50, editMode1);
 
     // Save button
-    if (GuiButton((Rectangle){710, 120, buttonWidth, 20}, GuiIconText(ICON_LENS , "")))
+    if (GuiButton((Rectangle){710, 120, buttonWidth, 20}, GuiIconText(ICON_LENS, "")))
     {
         std.id = atoi(id);
-        if(std.id  && std.average) {
-        SaveFormData(&std);
-        // insert(std);
+        if (std.id && std.average)
+        {
+            SaveFormData(&std);
+            // insert(std);
         }
     }
+
+    search_visualisation(10);
 }
 
-//Delete Form
+// Delete Form
 void delete_form()
 {
     student std = {0};
@@ -319,14 +317,13 @@ void delete_form()
     GuiSetStyle(LABEL, TEXT_ALIGNMENT, TEXT_ALIGN_CENTER);
 
     // Check if mouse is inside the first text box
-    if (IsMouseInsideTextBox((Rectangle){ labelWidth, 120, inputWidth, 20}))
+    if (IsMouseInsideTextBox((Rectangle){labelWidth, 120, inputWidth, 20}))
     {
         if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
         {
             editMode5 = !editMode5; // Toggle edit mode for TextBox 1
         }
     }
-
 
     // Draw text boxes
     int start = 0;
@@ -336,42 +333,74 @@ void delete_form()
     GuiTextBox(rec_input, id, 50, editMode1);
 
     // Save button
-    if (GuiButton((Rectangle){710, 120, buttonWidth, 20}, GuiIconText(ICON_CROSS , "")))
+    if (GuiButton((Rectangle){710, 120, buttonWidth, 20}, GuiIconText(ICON_CROSS, "")))
     {
         // std.id = atoi(id);
-        if(std.id  && std.average) {
-        // SaveFormData(&std);
-        // delete();
+        if (std.id && std.average)
+        {
+            // SaveFormData(&std);
+            // delete();
         }
     }
 }
 
-void DrawStudentTable() {
+void DrawStudentTable()
+{
     // Table headers
     GuiLabel((Rectangle){100, 120, 200, 30}, "ID");
     GuiLabel((Rectangle){300, 120, 200, 30}, "Name");
     GuiLabel((Rectangle){500, 120, 200, 30}, "Average");
 
     // Draw each row of the table
-    for (int i = 0; i < 12; i++) {
+    for (int i = 0; i < 12; i++)
+    {
         // ID
         int maxDigits = snprintf(NULL, 0, "%d", students[i].id);
-        char id[maxDigits] ;
-        snprintf(id,sizeof(id),"%d", students[i].id);
-        char avrage[5] ;
+        char id[maxDigits];
+        snprintf(id, sizeof(id), "%d", students[i].id);
+        char avrage[5];
         sprintf(avrage, "%.2f", students[i].average);
         // printf("%d\n", students[i].id);
         printf("%s\n", id);
-        GuiTextBox((Rectangle){100, 120+ (i+1) * 40, 200, 30}, id, 10, false);
+        GuiTextBox((Rectangle){100, 120 + (i + 1) * 40, 200, 30}, id, 10, false);
         // Name
-        GuiTextBox((Rectangle){300, 120 + (1+i) * 40, 200, 30}, students[i].name, 64, false);
+        GuiTextBox((Rectangle){300, 120 + (1 + i) * 40, 200, 30}, students[i].name, 64, false);
         // Average
-        GuiTextBox((Rectangle){500, 120 + (1+i) * 40, 200, 30}, avrage, 5, false);
+        GuiTextBox((Rectangle){500, 120 + (1 + i) * 40, 200, 30}, avrage, 5, false);
     }
-
 }
 
+void search_visualisation(int x)
+{
 
+        // Draw an arrow
+        Vector2 point1 = { screenWidth / 2, screenHeight / 2  };
+        Vector2 point2 = { screenWidth / 2 - 25, screenHeight / 2 - 50 };
+        Vector2 point3 = { screenWidth / 2 + 25, screenHeight / 2 - 50 };
+        DrawTriangle(point3, point2, point1, RED);
+
+    for (int i = 0; i < x; i++)
+    {
+        int blockWidth = 150;
+        int arrowWidth = 50;
+        
+        if(i != x - 1) {  
+        Rectangle bodyRect = {25 + i * (blockWidth + arrowWidth) + blockWidth, 273, 40, 4};
+        DrawRectangleRec(bodyRect, PINK);
+        Vector2 point1 = {25 + i * (blockWidth + arrowWidth) + blockWidth + 40, 270};
+        Vector2 point2 = {25 + i * (blockWidth + arrowWidth) + blockWidth + 40, 280};
+        Vector2 point3 = {25 + i * (blockWidth + arrowWidth) + blockWidth + 50, 275};
+        DrawTriangle(point1, point2, point3, PINK);
+        }
+
+        Rectangle rect = {25 + i * (blockWidth + arrowWidth), 250, blockWidth, 50};
+        DrawRectangleRec(rect, GRAY);
+    }
+
+    DrawRectangleRec((Rectangle){0,250, 25, 50}, LIGHTGRAY);
+    DrawRectangleRec((Rectangle){775,250, 25, 50}, LIGHTGRAY);
+    
+}
 
 void SaveFormData(student *formData)
 {
@@ -387,7 +416,6 @@ void SaveFormData(student *formData)
 int main()
 {
     openFile(&file, 'A');
-    
 
     // Initialization
 
@@ -433,4 +461,3 @@ int main()
 
     return 0;
 }
-
