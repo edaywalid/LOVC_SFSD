1d134cf9bcac0aeb12d30da2715f7ea48b00d018
edaywalid <becharwalid9@gmail.com>
2024-12-24 16:13:39 +0100

    updated build.bat file

--- build/build.bat
+++ build/build.bat
@@ -1,3 +1,4 @@
-gcc -o debug/myprogram main.c abstract.c index.c -Iinclude -LWlib -lraylib -lopengl32 -lgdi32 -lwinmm && .\debug\myprogram
+gcc -o debug/myprogram src/main.c src/abstract.c src/index.c -Iinclude -LWlib -lraylib -lopengl32 -lgdi32 -lwinmm && .\debug\myprogram
+---
 
 
